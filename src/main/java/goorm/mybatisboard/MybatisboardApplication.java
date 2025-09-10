5a1f48e784a0b01e33ec4928c7251d880810e3a5
kmgyu <lidersy961@gmail.com>
2025-09-10 09:16:04 +0900

    git init

---
+++ src/main/java/goorm/mybatisboard/MybatisboardApplication.java
@@ -0,0 +1,13 @@
+package goorm.mybatisboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MybatisboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MybatisboardApplication.class, args);
+    }
+
+}
