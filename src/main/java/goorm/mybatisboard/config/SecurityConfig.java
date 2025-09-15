4315ee0f614bb0bbf95abf4a35546028925e94b2
kmgyu <lidersy961@gmail.com>
2025-09-15 10:51:46 +0900

    config: add security config annotation

--- src/main/java/goorm/mybatisboard/config/SecurityConfig.java
+++ src/main/java/goorm/mybatisboard/config/SecurityConfig.java
@@ -1,13 +1,19 @@
 package goorm.mybatisboard.config;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@Slf4j
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity(prePostEnabled = true)
 @RequiredArgsConstructor
-@EnableWebMvc
 public class SecurityConfig {
   @Bean
   PasswordEncoder passwordEncoder() {
