d413947f12cf8536877249b39a11cec664ffaee7
kmgyu <lidersy961@gmail.com>
2025-09-15 11:10:51 +0900

    fix: temp package OS case insensitive issue

---
+++ src/main/java/goorm/mybatisboard/auth/dto2/LoginDto.java
@@ -0,0 +1,21 @@
+package goorm.mybatisboard.auth.dto2;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.Data;
+
+@Data
+public class LoginDto {
+    @Email(message = "{validation.email.invalid}")
+    @NotBlank(message = "{validation.email.required}")
+    private String email;
+
+    @Size(min=2, max=20, message = "{validation.nickname.size}")
+    @NotBlank(message = "{validation.nickname.required}")
+    private String nickname;
+
+    @Size(min=4, message = "{validation.password.size}")
+    @NotBlank(message = "{validation.password.required}")
+    private String password;
+}
